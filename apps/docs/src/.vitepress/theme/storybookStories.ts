870c45c2c688c7d4b7748317a0abf640985d0c13
romanbakurov <ronbakurov@gmail.com>
2026-07-20 22:59:15 +0200

    fix(docs): update storybook story ids

--- apps/docs/src/.vitepress/theme/storybookStories.ts
+++ apps/docs/src/.vitepress/theme/storybookStories.ts
@@ -2,15 +2,15 @@ export const STORYBOOK_STORIES = {
   'button.icons': 'primitives-button--with-icons',
   'button.matrix': 'primitives-button--matrix',
   'checkbox.states': 'primitives-checkbox--states',
-  'dropdown.groups': 'components-dropdown--with-groups',
-  'formField.complete': 'patterns-formfield--complete-example',
+  'dropdown.groups': 'components-dropdown--groups',
+  'formField.complete': 'patterns-formfield--with-input-context',
   'input.validation': 'primitives-input--validation',
-  'modal.basic': 'components-modal--basic',
+  'modal.basic': 'components-modal--default',
   'overview.web': 'overview-web--overview',
   'radioGroup.states': 'components-radiogroup--states',
-  'select.selection': 'components-select--selection',
+  'select.selection': 'components-select--simple-usage',
   'tabs.controlled': 'components-tabs--controlled',
-  'tooltip.triggers': 'components-tooltip--different-triggers',
+  'tooltip.triggers': 'components-tooltip--triggers',
 } as const;
 
 export type StorybookStoryKey = keyof typeof STORYBOOK_STORIES;
