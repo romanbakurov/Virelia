677b6c5b165d21e63057f750badcd6c986f345e9
romanbakurov <ronbakurov@gmail.com>
2026-07-20 21:30:57 +0200

    fix: keep radio group props visible to api docs

--- packages/react-native/src/components/RadioGroup/types.ts
+++ packages/react-native/src/components/RadioGroup/types.ts
@@ -1,2 +1,6 @@
+import type { RadioGroupProps as RadioGroupRootProps } from './Root';
+
 export type { RadioGroupItemProps } from './Item';
-export type { RadioGroupProps } from './Root';
+export type { RadioGroupProps as RadioGroupRootProps } from './Root';
+
+export type RadioGroupProps = RadioGroupRootProps;
