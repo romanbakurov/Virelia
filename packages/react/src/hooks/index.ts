a72890298e10db4d56a9b37064c576a8f33ff41d
romanbakurov <ronbakurov@gmail.com>
2026-07-20 16:46:06 +0200

    refactor: remove legacy overlay hooks

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,4 +1,1 @@
 export { useFloatingPosition } from './useFloatingPosition';
-export { useFocusManager } from './useFocusManager';
-export { useModalKeyboard } from './useModalKeyboard';
-export { useOutsideClick } from './useOutsideClick';
