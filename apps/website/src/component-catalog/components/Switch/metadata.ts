d853a9c8de8b8982ab12e990052296f462105030
Roman Bakurov <ronbakurov@gmail.com>
2026-08-22 13:41:34 +0200

    fix(website): define Switch component page metadata

---
+++ apps/website/src/component-catalog/components/Switch/metadata.ts
@@ -0,0 +1,11 @@
+import { defineComponentPageMetadata } from '../../metadata';
+
+export default defineComponentPageMetadata({
+  profile: 'form-control',
+  demo: {
+    staticProps: {
+      accessibilityLabel: "'Switch'",
+    },
+  },
+  related: ['checkbox', 'radio', 'form-field'],
+});
