d2576d5798d78c51c91cbf0189723f033a2bc9df
Roman Bakurov <ronbakurov@gmail.com>
2026-09-01 22:23:24 +0200

    feat(blog): add Blog V1 article route

---
+++ apps/website/src/app/(marketing)/(site)/blog/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import { notFound } from 'next/navigation';
+
+import {
+  getPublishedBlogArticle,
+  getPublishedBlogArticles,
+} from '@/blog';
+import { BlogArticleView } from '@/blog/ui';
+
+interface BlogArticlePageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  const articles = await getPublishedBlogArticles();
+
+  return articles.map((article) => ({
+    slug: article.slug,
+  }));
+}
+
+export default async function BlogArticlePage({
+  params,
+}: BlogArticlePageProps) {
+  const { slug } = await params;
+  const article = await getPublishedBlogArticle(slug);
+
+  if (!article) {
+    notFound();
+  }
+
+  return <BlogArticleView article={article} />;
+}
